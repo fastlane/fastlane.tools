34fc69aa953a1714af02555e796817a2518fed9f
David Ohayon <ohayon.1@gmail.com>
2017-07-12 16:06:23 -0400

    Update developer seconds saved with new metrics

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,52 @@
 (function(){
-
-  var url = "https://fastlane-refresher.herokuapp.com/duration";
   var startedWithMinutes = null;
 
+  // This number *never* changes
+  var developerSecondsSavedBeforeAnalyticIngester = 26304323354;
+
+  // These numbers are taken from our SQL query
+  var developerSecondsSavedAfterAnalyticIngester = 4292012226;
+  var developerSecondsSavedPerSecond = 560;
+  var timeUpdated = 1499885116;
+
   function reloadThis() {
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var results = JSON.parse(xmlhttp.responseText);
-        var seconds = 0;
-        for (var key in results) {
-          seconds += results[key];
-        }
+    // get current time
+    // get difference in seconds from timeUpdated
+    // multiply difference in seconds by developerSecondsSavedPerSecond
+    // add to sum of developerSecondsSavedBefore and After
 
-        minutes = seconds / 60; // to minutes
-        if (startedWithMinutes === null) {
-          startedWithMinutes = minutes;
-        }
-        var hours = minutes / 60; // to hours
+    var currentEpoch = ((new Date()).getTime()) / 1000;
 
-        var hoursAsArray = parseInt(hours).toString().split('');
-        var outputHtml = '';
-        var hoursOutput = [];
-        hoursAsArray.reverse();
+    var developerSecondsSavedSinceUpdate = (currentEpoch - timeUpdated) * developerSecondsSavedPerSecond;
+    var seconds = developerSecondsSavedSinceUpdate + developerSecondsSavedBeforeAnalyticIngester + developerSecondsSavedAfterAnalyticIngester;
 
-        for (var i = 0; i < hoursAsArray.length; i++) {
-          if ((i !== 0) && (i % 3 === 0)) {
-            hoursOutput[i] = '<span>'+ hoursAsArray[i] +'</span><span class="comma">,</span>';
-          }else{
-            hoursOutput[i] = '<span>'+ hoursAsArray[i] +'</span>';
-          }
-        }
+    var minutes = seconds / 60; // to minutes
+    if (startedWithMinutes === null) {
+      startedWithMinutes = minutes;
+    }
+    var hours = minutes / 60; // to hours
+    var hoursAsArray = parseInt(hours).toString().split('');
+    var outputHtml = '';
+    var hoursOutput = [];
+    hoursAsArray.reverse();
 
-        hoursOutput.reverse();
-        for (var a = 0; a < hoursOutput.length; a++) {
-          outputHtml += hoursOutput[a] ;
-        }
-
-        //document.getElementById("currentCount").innerHTML = parseInt(hours); // .toLocaleString()
-        document.getElementById('currentCount').innerHTML = outputHtml;
-
-        var diff = minutes - startedWithMinutes;
-        if (diff > 1) {
-          // document.getElementById("pageMinutes").innerHTML = diff
-          // document.getElementById("divTransparentMinutes").style.opacity = 1
-        }
-
-        // show everything
-        document.getElementById('timeCounter').style.opacity = 1;
+    for (var i = 0; i < hoursAsArray.length; i++) {
+      if ((i !== 0) && (i % 3 === 0)) {
+        hoursOutput[i] = '<span>'+ hoursAsArray[i] +'</span><span class="comma">,</span>';
+      }else{
+        hoursOutput[i] = '<span>'+ hoursAsArray[i] +'</span>';
       }
-    };
+    }
+
+    hoursOutput.reverse();
+    for (var a = 0; a < hoursOutput.length; a++) {
+      outputHtml += hoursOutput[a] ;
+    }
+
+    document.getElementById('currentCount').innerHTML = outputHtml;
+
+    // show everything
+    document.getElementById('timeCounter').style.opacity = 1;
   }
   reloadThis();
   setInterval(reloadThis, 10000);
@@ -59,3 +54,4 @@
   new WOW().init();
 
 })();
+
