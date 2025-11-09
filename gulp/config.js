5cc9b5b6680b604a6fadf18001e9f40e96a05b5a
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-11-08 20:12:37 -0500

    chore: store into dist

--- gulp/config.js
+++ gulp/config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
   Path: {
     SASS_SOURCES: './source/sass/**/*.{sass,scss}',
-    CSS_OUT_DIR: './build/assets/css/',
+    CSS_OUT_DIR: './dist/css/',
     JS_ENTRY: './source/js/main.js',
     JS_SOURCES: ['./source/js/main.js', './source/js/**/*.js'],
-    JS_OUT_DIR: './build/assets/js/'
+    JS_OUT_DIR: './dist/js/'
   }
 }
